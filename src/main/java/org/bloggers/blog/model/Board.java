1dbcd3b21c32cbd22661ca93628834a4f9592cce
sunny <parks38@uw.edu>
2021-03-08 20:39:41 +0900

    feat: board entity

---
+++ src/main/java/org/bloggers/blog/model/Board.java
@@ -0,0 +1,42 @@
+package org.bloggers.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //AI
+    private int id;
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob //대용량 데이터
+    private String content; //summer note library tool <html> 태그 섞어서 디자인
+
+    @ColumnDefault("0")
+    private int count; //조회수
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
+    //연관관계
+    @ManyToOne(fetch = FetchType.EAGER) // many = board, one = user
+    @JoinColumn(name="userId")
+    private User user; //작성자 - db 오브젝트를 저장할수 없다
+
+    @OneToMany(mappedBy = "board") // reply Board 가 FK
+    private List<Reply> replyList;
+}
