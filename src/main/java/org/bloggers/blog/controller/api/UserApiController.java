a2401485ab3bfd42cc6d6938fa0546224e56ac89
sunny <parks38@uw.edu>
2021-03-10 23:32:39 +0900

    feat :   user 회원가입 api controller

---
+++ src/main/java/org/bloggers/blog/controller/api/UserApiController.java
@@ -0,0 +1,26 @@
+package org.bloggers.blog.controller.api;
+
+import org.bloggers.blog.dto.ResponseDto;
+import org.bloggers.blog.model.RoleType;
+import org.bloggers.blog.model.User;
+import org.bloggers.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserApiController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/api/user")
+    public ResponseDto<Integer> save(@RequestBody User user){
+        System.out.println("UserApiController: save 호출됨 ");
+        // dependency injection
+        user.setRole(RoleType.USER);
+        int result = userService.회원가입(user);
+        return new ResponseDto<Integer>(HttpStatus.OK, result);
+    }
+}
