a386d47a82b61f32fb39a01c554e5224762cb1a9
sunny <parks38@uw.edu>
2021-03-14 20:41:50 +0900

    fix: updated login spring security token configuration

--- src/main/java/org/bloggers/blog/repository/UserRepository.java
+++ src/main/java/org/bloggers/blog/repository/UserRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    // select * from user where username = 1?
+    Optional<User> findByUsername(String username);
 }
