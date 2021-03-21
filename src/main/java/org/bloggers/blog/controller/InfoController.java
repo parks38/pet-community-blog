a1600c79afe9f1321f31309dee436aa4a650dba5
sunny <parks38@uw.edu>
2021-03-21 23:10:26 +0900

    feat: created info Controller

---
+++ src/main/java/org/bloggers/blog/controller/InfoController.java
@@ -0,0 +1,17 @@
+package org.bloggers.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InfoController {
+
+    //user 권한 필요
+    @GetMapping("/info")
+    public String infoBulletin() {
+        return "info/infoBulletin";
+    }
+
+
+    
+}
