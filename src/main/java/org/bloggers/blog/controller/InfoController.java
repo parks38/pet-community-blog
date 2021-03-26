f1f2284467830a2993b461f83f30d0cf7812d07f
sunny <parks38@uw.edu>
2021-03-26 18:08:51 +0900

    fix: updated main index controller

--- src/main/java/org/bloggers/blog/controller/InfoController.java
+++ src/main/java/org/bloggers/blog/controller/InfoController.java
@@ -1,17 +1,23 @@
 package org.bloggers.blog.controller;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class InfoController {
 
-    //user 권한 필요
-    @GetMapping("/info")
-    public String infoBulletin() {
-        return "board/infoBulletin";
+    @GetMapping({"","/"})
+    // 컨트롤러에서 세션 찾기 : @AuthenticationPrincipal PrincipalDetail principal
+    public String index(Model model,
+        @PageableDefault(size = 3, sort = "id",
+            direction = Sort.Direction.DESC) Pageable pageable) {
+        //model.addAttribute("boards", boardService.글목록(pageable));
+        return "index";
     }
-
 
     
 }
