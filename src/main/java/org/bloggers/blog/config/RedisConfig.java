d5208946c29e02049f97f52ee8aca427707f4343
sunny <parks38@uw.edu>
2021-04-18 21:59:48 +0900

    fix: redis session config connection

--- src/main/java/org/bloggers/blog/config/RedisConfig.java
+++ src/main/java/org/bloggers/blog/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.data.redis.RedisProperties.Jedis;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -13,10 +14,22 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
   @Value("${redis.host}")
   private String host;
+  @Value("${redis.port}")
+  private int port;
 
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
-    return new JedisConnectionFactory();
+    JedisConnectionFactory jedisConnectionFactory = null;
+    try {
+      RedisStandaloneConfiguration conn = new RedisStandaloneConfiguration();
+      conn.setHostName(host);
+      conn.setPort(port);
+
+      jedisConnectionFactory = new JedisConnectionFactory(conn);
+    } catch (Exception e) {
+
+    }
+    return jedisConnectionFactory;
   }
 
   @Bean
